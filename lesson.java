b890131b2c96f5a8630daaeb8ca81d0d7c1ce815
Merajuddin Sheikh <merajuddin786.s@gmail.com>
2018-12-21 19:04:10 +0530

    Add files via upload

---
+++ lesson.java
@@ -0,0 +1,27 @@
+package intigr;
+
+public class lesson {
+
+    public static void main (String[] args) {
+
+       // Addition of two Integer
+        int x, y , z;
+
+        x=10;
+        y=20;
+        z=x+y;
+
+        // Applying some arithmatic operation
+
+        int bal =(z-(x+y));
+
+        System.out.println("the sum of  x and y is "+ z);
+
+        System.out.println("the value is "+ bal);
+
+
+
+
+
+    }
+}
